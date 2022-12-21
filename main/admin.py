376412f67e2342a8c614bdc7db8867500f0ea7d1
Holden Karau <holden@pigscanfly.ca>
2022-12-21 09:56:59 -0800

    fvrhassan  gets cart working

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from main.models import Cart, Product, CartProduct
 
 # Register your models here.
+admin.site.register(Cart)
+admin.site.register(Product)
+admin.site.register(CartProduct)
