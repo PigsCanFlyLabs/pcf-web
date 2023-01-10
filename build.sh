30b3beba9b58692a0b5d10187df08b988909da63
Holden Karau <holden@pigscanfly.ca>
2023-01-09 21:24:06 -0800

    Try and fix gtag

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mypy .
 ./manage.py collectstatic --no-input
 # Hack, for now.
 cp -af ../cal-sync-magic/cal_sync_magic ./
-docker buildx build --platform=linux/amd64,linux/arm64 -t holdenk/pcfweb:v0.3.0a . --push
+docker buildx build --platform=linux/amd64,linux/arm64 -t holdenk/pcfweb:v0.3.0b . --push
 
