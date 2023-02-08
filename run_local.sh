0facf655a37a0dccac3dcb24e90bf85ecdefac33
Holden Karau <holden@pigscanfly.ca>
2023-02-07 23:11:24 -0800

    Allow local to magic.

--- run_local.sh
+++ run_local.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 if [ ! -f "cert.pem" ]; then
-  apt-get install -y mkcert
+  if ! command -v mkcert &> /dev/null; then
+    apt-get install -y mkcert
+  fi
   mkcert -cert-file cert.pem -key-file key.pem localhost 127.0.0.1
 fi
-python manage.py runserver_plus --cert-file cert.pem --key-file key.pem
+OAUTHLIB_RELAX_TOKEN_SCOPE=1 python manage.py runserver_plus --cert-file cert.pem --key-file key.pem
